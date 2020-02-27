cdb96b99f5b4809a86a5a5e9abd990baf58df1df
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-02-28 03:53:57 +0900

    Initial commit

---
+++ selenium.js
@@ -0,0 +1,106 @@
+const webdriver = require('selenium-webdriver');
+const browserstack = require('browserstack-local');
+require('dot-env');
+const proxyHost = process.env.PROXY_HOST;
+const proxyPort = process.env.PROXY_PORT;
+const bsUser = process.env.BS_USER;
+const bsKey = process.env.BS_KEY;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+});
+
+// creates an instance of Local
+const bs_local = new browserstack.Local();
+
+// replace <browserstack-accesskey> with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+const bs_local_args = {
+  'key': bsKey
+};
+if (proxyHost !== '') {
+    bs_local_args.proxyHost = proxyHost;
+}
+if (proxyPort !== '') {
+    bs_local_args.proxyPort = proxyPort;
+}
+
+async function run() {
+  // starts the Local instance with the required arguments
+  await new Promise((resolve) => {
+    bs_local.start(bs_local_args, () => {
+      resolve();
+    });
+  });
+  // Input capabilities
+  const capabilities = {
+    'browserName' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '10',
+    'resolution' : '1024x768',
+    'browserstack.user' : bsUser,
+    'browserstack.key' : bsKey,
+    'browserstack.local' : 'true',
+    'name' : 'Bstack-[Node] :Sample Test'
+  };
+  let driver = new webdriver.Builder().
+    usingServer('http://hub-cloud.browserstack.com/wd/hub').
+    withCapabilities(capabilities);
+  if (proxyHost !== '') {
+    let proxy = `http://${proxyHost}`;
+    if (proxyPort !== '') {
+      proxy = `${proxy}:${proxyPort}`;
+    }
+    driver = driver.usingWebDriverProxy(proxy);
+  }
+  driver = driver.build();
+
+  driver.get('http://localhost:3000/').then(() => {
+    driver.getTitle().then((title) => {
+      console.log(title);
+      driver.quit();
+    });
+  });
+}
+
+run();
+
+
+
+
+/*var driver = new webdriver.Builder().
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  withCapabilities(capabilities).
+  usingWebDriverProxy("http://172.16.231.132:15520").
+  build();
+
+driver.get('http://localhost:63342/Maplat/index.html?_ijt=npe7ic75hpl4apu1l04smak2mi#!s:morioka_ndl2/b:osm/x:-7105760.976961669/y:8695999.197015667/z:2.1459301282852747').then(function(){
+  //driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function(){
+    driver.getTitle().then(function(title) {
+      console.log(title);
+      driver.quit();
+    });
+  //});
+});
+
+// check if BrowserStack local instance is running
+//console.log(bs_local.isRunning());
+
+// stop the Local instance
+/*bs_local.stop(function() {
+  console.log("Stopped BrowserStackLocal");
+});*/
+
+
+
+
+
+
+
+
